0e800b42454d60f41a35e0f97e4588a8076deab5
youyi <youyi831123@163.com>
2021-12-17 14:07:33 +0800

    init project

---
+++ workflow/src/App.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Date: 2021-12-17 13:57:34
+ * @LastEditors: youyi
+ * @LastEditTime: 2021-12-17 14:05:15
+-->
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ workflow/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Date: 2021-12-17 13:57:34
+ * @LastEditors: youyi
+ * @LastEditTime: 2021-12-17 14:04:10
+-->
+<template>
+  <div class="hello"></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
